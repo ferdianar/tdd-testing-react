c070a69a701a7f2697390a563c2431bc27586dbb
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-07 01:40:12 +0700

    update button and counter value

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PrimaryButton: React.FunctionComponent = () => {
        return (
               <React.Fragment>
-                     <button className='w-fit py-2 px-4 text-sm font-medium rounded-md bg-blue-700 text-white hover:bg-blue-900'>Increment Button</button>
+                     <button id='primary-button' className='mr-1 w-fit py-2 px-4 text-sm font-medium rounded-md bg-blue-700 text-white hover:bg-blue-900'>Primary Button</button>
               </React.Fragment>
        )
 }
@@ -11,7 +11,7 @@ const PrimaryButton: React.FunctionComponent = () => {
 const SecondaryButton: React.FunctionComponent = () => {
        return (
               <React.Fragment>
-                     <button className='w-fit py-2 px-4 text-sm font-medium rounded-md bg-blue-50 text-blue-600 hover:bg-blue-200'>Increment Button</button>
+                     <button id='secondary-button' className='secondary-button ml-1 w-fit py-2 px-4 text-sm font-medium rounded-md bg-blue-50 text-blue-600 hover:bg-blue-100'>Secondary Button</button>
               </React.Fragment>
        )
 }
